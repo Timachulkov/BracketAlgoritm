1c9127f055d6332c474b7f4acbe0f5d53ccb046d
Tima <timachulkov1@gmail.com>
2022-08-16 15:09:16 +0500

    Добавьте файлы проекта.

---
+++ BracketAlgoritm/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+using BracketAlgoritm;
+
+string line;
+Console.WriteLine("Hello, World!");
+line = Console.ReadLine();
+ValidBracket validBracket = new ValidBracket();
+Console.WriteLine(validBracket.IsValid(line));
---
+++ BracketAlgoritm/ValidBracket.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BracketAlgoritm
+{
+    internal class ValidBracket
+    {
+        public bool IsValid(string line)
+        {
+            Stack<char> stack = new Stack<char>();
+            foreach(char i in line)
+            {
+                switch(i)
+                {
+                    case '{':
+                        stack.Push('}');
+                        break;
+                    case '[':
+                        stack.Push(']');
+                        break;
+                    case '(':
+                        stack.Push(')');
+                        break;
+                    case '}':
+                    case ']':
+                    case ')':
+                        if (stack.Count == 0) return false;
+                        if (stack.Pop() != i) return false;
+                        break;
+                     default:
+                        break;
+                }
+            }
+            return stack.Count == 0;
+        }
+    }
+}
